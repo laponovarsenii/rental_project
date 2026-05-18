68886b3dc869381ac5ccb51f24e47dbbb368ea67
Arsenii <arseniilaponov6@gmail.com>
2026-05-18 17:21:41 +0300

    Fixing chat/urls

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ChatCreateView, MessageCreateView, ChatMessagesListView
 
 urlpatterns = [
-    path('chat/create/', ChatCreateView.as_view()),
-    path('chat/message/', MessageCreateView.as_view()),
-    path('chat/<int:chat_id>/messages/', ChatMessagesListView.as_view()),
+    path('create/', ChatCreateView.as_view(), name='chat-create'),
+    path('message/', MessageCreateView.as_view(), name='chat-message'),
+    path('<int:chat_id>/messages/', ChatMessagesListView.as_view(), name='chat-messages'),
 ]
